b5eb8b6e88e5cb87f83df594c2b101d16694779b
Oussama Danba <oussama@danba.nl>
2021-03-20 19:38:59 +0100

    Make network Brother laser printer work (gutenprint maybe not needed)

--- configuration.nix
+++ configuration.nix
@@ -40,6 +40,11 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.gutenprint pkgs.gutenprintBin pkgs.brlaser ];
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
 
   # Enable sound through pulseaudio.
   sound.enable = true;
