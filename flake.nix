509f345d539b48207af2372fcc236e50d71f023d
Oussama Danba <oussama@danba.nl>
2024-04-15 11:51:34 +0200

    lemuria: remove permanently

--- flake.nix
+++ flake.nix
@@ -53,37 +53,5 @@
         ./machines/vale/configuration.nix
       ];
     };
-    homeConfigurations.lemuria = home.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [
-        ./home-manager/common-cli.nix
-        ./home-manager/common-gui.nix
-        ./home-manager/dev-tools-cli.nix
-        ./home-manager/gnome-extensions.nix
-        {
-          xsession.enable = true;
-          programs.home-manager.enable = true;
-          home = {
-            username = "odanba";
-            homeDirectory = "/home/odanba";
-            packages = with pkgs; [
-              google-chrome
-              htop
-              python3Packages.pyserial
-              remmina
-              slack
-              sshfs
-            ];
-
-            stateVersion = "23.11";
-          };
-
-          programs.git = {
-            userEmail = "oussama.danba@pqshield.com";
-            userName = "Oussama Danba";
-          };
-        }
-      ];
-    };
   };
 }
