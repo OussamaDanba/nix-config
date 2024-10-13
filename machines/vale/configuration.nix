1d29b45a4b70ae2a4fad35b67fce2c922b2d3fcb
Oussama Danba <oussama@danba.nl>
2024-10-13 11:44:29 +0200

    update homeassistant

--- machines/vale/configuration.nix
+++ machines/vale/configuration.nix
@@ -157,7 +157,7 @@ in {
       volumes = ["home-assistant:/config"];
       environment.TZ = "Europe/Amsterdam";
       # Note: Tag needs to change in order for it to be updated
-      image = "ghcr.io/home-assistant/home-assistant:2024.7.3";
+      image = "ghcr.io/home-assistant/home-assistant:2024.10.2";
       extraOptions = [
         "--network=host"
         # Needed to make DHCP discovery work
