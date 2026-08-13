66a8c6caef10f9781cec7157fbead56e13105eae
Oussama Danba <oussama@danba.nl>
2026-08-13 16:12:58 +0200

    update

--- machines/vale/configuration.nix
+++ machines/vale/configuration.nix
@@ -166,7 +166,7 @@ in {
       volumes = ["home-assistant:/config"];
       environment.TZ = "Europe/Amsterdam";
       # Note: Tag needs to change in order for it to be updated
-      image = "ghcr.io/home-assistant/home-assistant:2026.6.4";
+      image = "ghcr.io/home-assistant/home-assistant:2026.8.1";
       extraOptions = [
         "--network=host"
         # Needed to make DHCP discovery work
@@ -176,7 +176,7 @@ in {
     containers.esphome = {
       volumes = ["esphome:/config"];
       # Note: Tag needs to change in order for it to be updated
-      image = "ghcr.io/esphome/esphome:2026.6.1";
+      image = "ghcr.io/esphome/esphome:2026.7.4";
       ports = ["0.0.0.0:6052:6052"];
     };
   };
